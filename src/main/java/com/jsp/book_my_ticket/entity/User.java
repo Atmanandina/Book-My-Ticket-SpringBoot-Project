48e24eace7591a2b5d4bfe10b7490c1dc755de53
Atmanandin <atmanandinamati@gmail.com>
2025-12-01 10:29:42 +0530

    Redis OTP Verification

--- src/main/java/com/jsp/book_my_ticket/entity/User.java
+++ src/main/java/com/jsp/book_my_ticket/entity/User.java
@@ -1,14 +1,19 @@
 package com.jsp.book_my_ticket.entity;
 
 import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/jsp/book_my_ticket/service/UserService.java
+++ src/main/java/com/jsp/book_my_ticket/service/UserService.java
@@ -7,12 +7,17 @@ import com.jsp.book_my_ticket.dto.LoginDto;
 import com.jsp.book_my_ticket.dto.UserDto;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 
 public interface UserService {
-	String register(UserDto userDto, BindingResult result);
+	String register(UserDto userDto, BindingResult result,RedirectAttributes attributes);
 
 	String login(LoginDto dto, RedirectAttributes attributes,HttpSession session);
 
 	String logout(HttpSession session, RedirectAttributes attributes);
+
+	
+
+	String submitOtp(int otp, String email, RedirectAttributes attributes);
 }
