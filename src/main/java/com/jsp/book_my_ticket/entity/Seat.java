c5f35dca5beea18b1dcd2211c04b9fe6928bfe1f
Atmanandin <atmanandinamati@gmail.com>
2025-12-08 18:28:16 +0530

    Manage Seat Layout

---
+++ src/main/java/com/jsp/book_my_ticket/entity/Seat.java
@@ -0,0 +1,21 @@
+package com.jsp.book_my_ticket.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Seat {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String seatNumber;
+	private Character seatRow;
+	private Integer seatColumn;
+	private String category;
+
+}
